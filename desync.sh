deaecff3e8f71d770b5594c037e24872d9e2be52
Haydon Murdoch <hgt.murdoch@gmail.com>
2017-04-14 21:32:32 +0100

    Implement some changes as per shellcheck's suggestions
     - Remove "function" keywords from each function
     - Use `echo ... | wc -l` instead of here-string

--- desync.sh
+++ desync.sh
@@ -3,24 +3,24 @@
 # desync.sh - decrypt one or more external drives
 #             and sync files between them
 
-function not_root () {
+not_root () {
     echo "Not running as root, unable to continue"
     exit 1
 }
 
-function keys_file_no_exist () {
+keys_file_no_exist () {
     echo "$keys_file not found"
     echo "Please create $keys_file and populate it with keys"
     exit 1
 }
 
-function keys_file_incorrect_mode () {
+keys_file_incorrect_mode () {
     echo "$keys_file was found to have mode $actual_mode"
     echo "Please run: (sudo) chmod $expected_mode $keys_file"
     exit 1
 }
 
-function no_external_drives () {
+no_external_drives () {
     echo "I could not find any drives with unmounted partitions"
     exit 1
 }
@@ -48,4 +48,4 @@ drives=$(lsblk --noheadings --raw -o NAME,MOUNTPOINT |  # grab all disks/parts
 
 [ -z "$drives" ] && no_external_drives  # Exit if $drives is zero-length
 
-echo "Assuming $(wc -l <<< "$drives") drive(s), is this correct?"
+echo "Assuming $(echo "$drives" | wc -l) drive(s), is this correct?"
