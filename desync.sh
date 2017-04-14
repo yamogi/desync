f56e402abda4d2c670c997cb6934f898f746d401
Haydon Murdoch <hgt.murdoch@gmail.com>
2017-04-14 21:05:41 +0100

    Add some basic functions
     - Check script being run as root
     - Check custom keys file exists
     - Check custom keys file has correct mode (still in progress)
     - Get number of drives with unmount partitions (used for
       estimating how many external drives are attached)

--- desync.sh
+++ desync.sh
@@ -29,3 +29,42 @@
 #  6. Once initial rsync is complete, rsync again (unsure of source yet) onto
 #     each separate drive
 #  7. When all rsyncs are complete, unmount all external drives and luksClose
+
+function not_root () {
+    echo "Not running as root, unable to continue"
+    exit 1
+}
+
+function keys_file_no_exist () {
+    echo "$keys_file not found"
+    echo "Please create $keys_file and populate it with keys"
+    exit 1
+}
+
+function keys_file_incorrect_mode () {
+    echo "Blah"
+    exit 1
+}
+
+# Check running as root
+(( $EUID == 0 )) || not_root
+
+# Check keys file exists
+keys_file="/usr/local/etc/keys"
+[ -f "$keys_file" ] || keys_file_no_exist
+
+# Check keys file has correct mode
+expected_mode="600"
+#(( $(stat -c %a "$keys_file" == "$expected_mode") )) || keys_file_incorrect_mode
+# Above not working yet...
+
+# Get list of drives that contain unmounted partitions
+drives=$(lsblk --noheadings --raw -o NAME,MOUNTPOINT |  # grab disks/parts
+         awk '$1~/sd.[[:digit:]]/ && $2 == ""' |        # get sd<number> with no mountpoint
+         tr -d '[:blank:]' |                            # remove blank characters
+         sed 's/.$//' |                                 # remove last character (sdb1 > sdb)
+         sort |                                         # sort
+         uniq                                           # remove duplicates
+)
+
+echo "Assuming $(wc -l <<< "$drives") drive(s), is this correct?"
