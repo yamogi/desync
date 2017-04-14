2628550db40394e61266c8463a35aa1ea9abc896
Haydon Murdoch <hgt.murdoch@gmail.com>
2017-04-14 21:11:16 +0100

    Move general plan to readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Test
+## General plan:
+1. Ensure decryption keys are stored in the file /usr/local/etc/keys (mode 600), one key per line:
+
+```
+# cat /usr/local/etc/keys
+key1
+key2
+...
+```
+
+```
+$ cat /usr/local/etc/keys
+cat: /usr/local/etc/keys: Permission denied
+```
+
+2. Ensure script is being run as root (for reading keys file)
+3. Try and work out how many drives are connected (parse fdisk or lsblk?)
+4. Let user know how many external drives it thinks are attached and wait for confirmation
+5. Use /usr/local/etc/keys and loop through drives, decrypting all with cryptsetup
+6. Set some sort of trap in the script that luksCloses all drives if ^C is triggered?
+7. Make necessary /mnt directories and mount all drives
+8. rsync files from specified path onto first drive (dry-run first then wait for confirmation?)
+9. Once initial rsync is complete, rsync again (unsure of source yet) onto each separate drive
+10. When all rsyncs are complete, unmount all external drives and luksClose
--- desync.sh
+++ desync.sh
@@ -3,33 +3,6 @@
 # desync.sh - decrypt one or more external drives
 #             and sync files between them
 
-# General plan:
-#  0. Ensure decryption keys are stored in the file /usr/local/etc/keys
-#     (mode 600), one key per line:
-#
-#     # cat /usr/local/etc/keys
-#     key1
-#     key2
-#     ...
-#
-#     $ cat /usr/local/etc/keys
-#     cat: /usr/local/etc/keys: Permission denied
-#
-#  0.5. Ensure script is being run as root (for reading keys file)
-#  1. Try and work out how many drives are connected (parse fdisk or lsblk?)
-#  2. Let user know how many external drives it thinks are attached and
-#     wait for confirmation
-#  3. Use /usr/local/etc/keys and loop through drives, decrypting all with
-#     cryptsetup
-#  3.5. Set some sort of trap in the script that luksCloses all drives if
-#       ^C is triggered?
-#  4. Make necessary /mnt directories and mount all drives
-#  5. rsync files from specified path onto first drive (dry-run first then
-#     wait for confirmation?)
-#  6. Once initial rsync is complete, rsync again (unsure of source yet) onto
-#     each separate drive
-#  7. When all rsyncs are complete, unmount all external drives and luksClose
-
 function not_root () {
     echo "Not running as root, unable to continue"
     exit 1
