2a1049afd82eaca66a7b470e05dba3f9c105b00d
Haydon Murdoch <hgt.murdoch@gmail.com>
2017-04-15 19:22:19 +0100

    Formatting changes
     - Use printf instead of echo in most places
     - Comment out call to check_arguments for the time being

--- desync.sh
+++ desync.sh
@@ -7,13 +7,12 @@
 # FUNCTIONS #
 #############
 not_root () {
-    echo "Please run this script as root"
+    printf %s\\n "Please run this script as root"
     exit 1
 }
 
 check_arguments () {
     [ "$#" = 0 ] && usage  # Show usage (and exit) if no further arguments
-
 }
 
 usage () {
@@ -24,24 +23,19 @@ usage () {
 }
 
 keys_file_no_exist () {
-    echo "$keys_file not found"
-    echo "Please create $keys_file and populate it with keys"
+    printf %s\\n "$keys_file not found"
+    printf %s\\n "Please create $keys_file and populate it with keys"
     exit 1
 }
 
 keys_file_incorrect_mode () {
-    echo "$keys_file was found to have mode $actual_mode"
-    echo "Please run: (sudo) chmod $expected_mode $keys_file"
+    printf %s\\n "$keys_file was found to have mode $actual_mode"
+    printf %s\\n "Please run: (sudo) chmod $expected_mode $keys_file"
     exit 1
 }
 
-get_length_of_keys_file () {
-    length_of_keys_file=$(wc -l < "$keys_file")
-    echo "  Keys file contains: $length_of_keys_file line(s)"
-}
-
 decrypt_drives () {
-    echo "Something will be here eventually..."
+    printf %s\\n "Something will be here eventually..."
 }
 
 ##########
@@ -49,24 +43,30 @@ decrypt_drives () {
 ##########
 # Check running as root
 [ "$(id -u)" = 0 ] || not_root
-echo "Running as root..."
-echo
+printf %s\\n "Running as root..."
+printf %s\\n
 
-check_arguments
+#check_arguments
 
 # Check keys file exists
 keys_file="/usr/local/etc/keys"
-echo "Keys file: $keys_file"
+printf %s\\n "Checking properties of file: $keys_file"
+printf %s\\n "\\ Checking for existence of file"
 [ -f "$keys_file" ] || keys_file_no_exist
-echo "  Test 1 passed: Keys file exists..."
+printf %s\\n "  \\ Keys file exists"
 
 # Check keys file has correct mode
 expected_mode="600"
 actual_mode=$(stat -c %a "$keys_file")
+printf %s\\n "\\ Checking permissions of file"
 [ "$actual_mode" = "$expected_mode" ] || keys_file_incorrect_mode
-echo "  Test 2 passed: Keys file has correct permissions ($expected_mode)..."
+printf %s\\n "  \\  Keys file has correct permissions ($expected_mode)"
 
-get_length_of_keys_file
+printf %s\\n "\\ Getting length of file: $keys_file"
+length_of_keys_file=$(wc -l < "$keys_file")
+printf %s\\n "  \\ Keys file contains: $length_of_keys_file line(s)"
+
+echo  # Newline
 
 ###########
 # OPTIONS #
@@ -97,8 +97,8 @@ done
 while true; do
     read -r -p "Do you wish to continue (y/n)? " choice
     case $choice in
-        [Yy]|yes ) echo "Yes picked" ; decrypt_drives ; break;;
-        [Nn]|no ) echo "No picked" ; exit 1;;
-        * ) echo "Please answer Y/y/yes or N/n/no.";;
+        [Yy]|yes ) printf %s\\n "Yes picked" ; decrypt_drives ; break;;
+        [Nn]|no ) printf %s\\n "No picked" ; exit 1;;
+        * ) printf %s\\n "Please answer Y/y/yes or N/n/no.";;
     esac
 done
