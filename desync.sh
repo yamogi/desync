9c4b18e3401706f30822225e7c10c7a782d557f6
Haydon Murdoch <hgt.murdoch@gmail.com>
2017-04-15 15:09:27 +0100

    Some refactoring
     - Include usage function, and call it to exit if necessary
     - No longer try to guess which partitions to use, request from
       user instead
     - Use getopts to get directory and partitions from user

--- desync.sh
+++ desync.sh
@@ -11,6 +11,13 @@ not_root () {
     exit 1
 }
 
+usage () {
+    printf %s\\n "Usage $(basename "$0") -d [directory] partition1 [partition2...]"
+    printf %s\\n " - A valid directory must be specified"
+    printf %s\\n " - At least one partition must be specified"
+    exit 1
+}
+
 keys_file_no_exist () {
     echo "$keys_file not found"
     echo "Please create $keys_file and populate it with keys"
@@ -27,27 +34,15 @@ get_length_of_keys_file () {
     echo "  Keys file contains: $(wc -l < "$keys_file") line(s)"
 }
 
-no_unmounted_partitions () {
-    echo "No unmounted partitions found"
-    exit 1
-}
-
 decrypt_drives () {
-    echo "Preparing to decrypt drives..."
-    loop=1
-    for drive in $(echo "$partitions" | sed 's/.$//' | sort | uniq); do
-        echo "  Checking /dev/${drive}1 is a valid LUKS device..."
-        cryptsetup luksDump /dev/${drive}1 2>&1 >/dev/null || continue
-        echo "  Attempting to decrypt /dev/${drive}1 with key found on line $loop of $keys_file"
-        passphrase=$(sed "${loop}p" "$keys_file")
-        echo "$passphrase" | cryptsetup luksOpen /dev/"$drive"1 drive"$loop"
-        loop=$((loop+1))
-    done
+    echo "Something will be here eventually..."
 }
 
 ##########
 # CHECKS #
 ##########
+[ "$#" = 0 ] && usage  # Show usage if no arguments
+
 # Check running as root
 [ "$(id -u)" = 0 ] || not_root
 echo "Running as root..."
@@ -67,41 +62,30 @@ echo "  Test 2 passed: Keys file has correct permissions ($expected_mode)..."
 
 get_length_of_keys_file
 
-echo ; echo
+###########
+# OPTIONS #
+###########
+directory=
 
-#########
-# START #
-#########
-# Get list of unmounted partitions
-partitions=$(lsblk --noheadings --raw -o NAME,MOUNTPOINT |         # grab all disks/parts
-             #awk '$1~/sd.[[:digit:]]/ { print $1 }' |              # get any sd<number> ... (testing)
-             awk '$1~/sd.[[:digit:]]/ && $2 == "" { print $1 }' |  # get any sd<number> with no mountpoint
-             tr -d '[:blank:]' |                                   # remove any blank characters
-             sort                                                  # sort
-)
+while getopts "d:" opt; do
+    case $opt in
+        d)
+            [ -d "$OPTARG" ] || usage  # Exit if not a directory
+            directory=$OPTARG
+            printf %s\\n "$directory is a valid directory"
+            ;;
+    esac
+done
 
-[ -z "$partitions" ] && no_unmounted_partitions  # Exit if $partitions is zero-length
+[ -z "$directory" ] && usage
 
-# Inform the user about discovered unmounted partitions
-echo "Found $(echo "$partitions" | wc -l) unmounted partitions:"
+shift $((OPTIND-1))
+
+partitions=$*
+printf %s\\n "Showing partitions..."
 for partition in $partitions; do
-    echo "  /dev/$partition"
+    printf %s\\n "$partition"
 done
-echo
-
-# Get size of drives that contain unmounted partitions
-#  - Drives are collapsed when possible - e.g. when presented with sdb1 and sdb2,
-#    the script will simply check the overall size of /dev/sdb
-#  - This is subject to change in the future
-for drive in $(echo "$partitions" | sed 's/.$//' | sort | uniq); do
-    echo "  Getting size of /dev/$drive..."
-    echo "    $(lsblk --noheadings --raw -o NAME,SIZE /dev/"$drive" \
-                 | head -n1 \
-                 | awk '{ print $NF }')"
-    echo
-done
-
-echo "Will assume first partition of each drive"
 
 # Get user input to continue
 while true; do
